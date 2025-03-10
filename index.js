45bfec61b381970f67633a9f9ad6061766a5afd1
nathanx-bd <166464065+nathanx-bd@users.noreply.github.com>
2025-03-10 04:53:59 +0000

    Deploying to gh-pages from @ nathanx-bd/ci-cd-example@2dc09f5d1bc12881a38560874345acbf04faedab 🚀

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// TypeScript logic for button click
+const button = document.getElementById("clickMeButton");
+button.addEventListener("click", () => {
+    alert("Hello, thanks for clicking!");
+});
